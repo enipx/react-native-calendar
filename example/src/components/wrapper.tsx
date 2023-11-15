021cee62990f6f7ab6b77fbcb29d692e83c0d741
enipx <progenipx@gmail.com>
2023-11-15 12:37:31 +0100

    chore: improvements

--- example/src/components/wrapper.tsx
+++ example/src/components/wrapper.tsx
@@ -1,4 +1,4 @@
-import { Text, View, type ViewProps } from 'react-native';
+import { StyleSheet, Text, View, type ViewProps } from 'react-native';
 
 interface WrapperProps extends ViewProps {
   title?: string;
@@ -8,32 +8,29 @@ export const Wrapper = (props: WrapperProps) => {
   const { title, ...rest } = props;
 
   return (
-    <View
-      style={{
-        paddingBottom: 24,
-        borderBottomColor: 'rgba(0,0,0,.1)',
-        borderBottomWidth: 1,
-        marginBottom: 32,
-      }}
-    >
-      {title && (
-        <Text
-          style={{
-            height: 44,
-            color: '#fff',
-            backgroundColor: '#8A72FB',
-            textAlign: 'center',
-            paddingTop: 8,
-            marginHorizontal: 16,
-            marginBottom: 16,
-            fontSize: 18,
-            fontWeight: 'bold',
-          }}
-        >
-          {title}
-        </Text>
-      )}
+    <View style={styles.container}>
+      {title && <Text style={styles.title}>{title}</Text>}
       <View {...rest} />
     </View>
   );
 };
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingBottom: 24,
+    borderBottomColor: 'rgba(0,0,0,.1)',
+    borderBottomWidth: 1,
+    marginBottom: 32,
+  },
+  title: {
+    height: 44,
+    color: '#fff',
+    backgroundColor: '#8A72FB',
+    textAlign: 'center',
+    paddingTop: 8,
+    marginHorizontal: 16,
+    marginBottom: 16,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
